89cef5414504740b74324079ff048a79ce6f524f
Theophile <theophile.guillet5@gmail.com>
2022-06-16 20:00:09 +0400

    First Commit

---
+++ Variables.c
@@ -0,0 +1,15 @@
+int Variable()
+{
+    // Variable made from characters
+    // [] = multiple characters
+    char Name [] = "value";
+
+    // Variable made of integer
+    int Number = 35;
+
+    //%s = add string(variable) on the print function + "," and name of the variable which will replace the %s
+    printf("information %s\n", Name);
+    //%d = add integer(variable) on the print function + "," and name of the variable which will replace the %d
+    printf("information %d\n", Number);
+    return;
+}
